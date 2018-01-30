f23e83d60eae7374b68155bbcc14193533c4f6d1
Zach Cox <zach@Zachs-MacBook-Pro.local>
2018-01-29 20:55:15 -0600

    Added some requirements for creating a heroku app.

---
+++ hello.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/[YOUR_DATABASE_NAME]'
+db = SQLAlchemy(app)
+
+
+@app.route('/')
+def hello():
+        return 'Hello World'
