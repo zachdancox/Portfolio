fb813e5fb1fcacaa2b227bb4c30975316f476137
Zach Cox <zach@Zachs-MacBook-Pro.local>
2018-01-29 21:59:42 -0600

    Moved some stuff around.

--- hello.py
+++ hello.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template, request, url_for
 import os
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, url_for
+
 app = Flask(__name__)
 
 
-@app.route("/")
-def show_index():
-        return render_template("index.html")
+@app.route('/')
+def home():
+        return render_template('index.html')
+
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+    app.run(debug=True)
 
