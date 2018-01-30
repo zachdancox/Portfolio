01a5cbf97f0886f5c059578ae4798b0b8d53c4da
Zach Cox <zach@Zachs-MacBook-Pro.local>
2018-01-29 21:47:17 -0600

    Work

--- hello.py
+++ hello.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template, request, url_for
 import os
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/[YOUR_DATABASE_NAME]'
-db = SQLAlchemy(app)
 
 
 @app.route('/')
 @app.route('/index')
 def show_index():
         return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
+
